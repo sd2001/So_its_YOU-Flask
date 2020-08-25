1c6a950d06ae1bbd818ca6e68b7ff5f476ce2551
sd2001 <im.swarnabha2001@gmail.com>
2020-08-25 19:04:35 +0530

    Fixing app routes

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from pymongo import MongoClient
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash,check_password_hash
 import bcrypt
-#from flask_notifications import Notifications
+from flask_login import login_user,current_user
 
 client=MongoClient('mongodb+srv://swarnabha:swarnabhadb@cluster0.yfmwo.mongodb.net/Logbase?retryWrites=true&w=majority')
 db=client['Users']
 
 
 app=Flask(__name__)
+name=""
 
 @app.route('/')
 def home():
@@ -29,16 +30,22 @@ def login_post():
     password=request.form.get('pass')
     data=db.credentials
     flag=False
+    
     for i in data.find():
         if i['email']==email:
+            name=i['username']
             flag=True
-            if check_password_hash(i['password'], password):                
+            if check_password_hash(i['password'], password):                               
                 return redirect(url_for('profile'))
             
               
              
-    if flag == False:
-        flash("Check your credentials and try again")
+    if email is None and password is None:
+        flash("Please fill both the fields and Try Again")
+        return redirect(url_for('login'))
+    
+    if flag==False:
+        flash("Invalid Credentials")
         return redirect(url_for('login'))
     
     return redirect(url_for('login'))
@@ -62,7 +69,7 @@ def reg_post():
     for i in data.find():
         if i['email']==email:  
             flash("Your Email Address is already registered with us")
-            return redirect(url_for('reg')) 
+            return redirect(url_for('login')) 
     else:   
         user_info={'username': name,
                 'email': email,
